f7a6c80c1edcf4c49f0e9d7c91f27dee0f96b41e
RemousArkarou <dds0022@edu.rocwb.nl>
2024-02-29 11:26:33 +0100

    press 'Q' to stop program

--- rollercoaster-check-main/main.py
+++ rollercoaster-check-main/main.py
@@ -37,5 +37,5 @@ while running:
         print("hello everyone, I am here now")
 
     result = input("Druk op Enter om nog een keer te checken, of X om te stoppen\n\n")
-    if(result.upper() == "X"):
+    if(result.upper() == "Q"):
         running = False
